12e88b6f9f854d05178ec04733cbaed3a299a1e3
wys <2385758031@qq.com>
2019-01-07 15:36:23 +0800

    tiwen

--- wheel/src/router/index.js
+++ wheel/src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Faq from "../components/question.vue";
+import index from "../page/index.vue";
 Vue.use(Router);
 export default new Router({
   routes: [
+    {
+      path: "/index",
+      component: index
+    },
     {
       path: "/faq",
       component: Faq
