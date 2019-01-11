97265f1b0345934010ee8804e6656da1658a945a
wys <2385758031@qq.com>
2019-01-11 14:06:37 +0800

    user

---
+++ canves/src/api/index.js
@@ -0,0 +1,9 @@
+// export let uploadBase64 = base64 => {
+//   return sendRequest("http://123.206.55.50:11000/upload_base64", "POST", {
+//     base64
+//   });
+// };
+// 图片转成base64
+// export let imgToBase64 = url => {
+//   return sendRequest("http://123.206.55.50:11000/tobase64", "POST", { url });
+// };
